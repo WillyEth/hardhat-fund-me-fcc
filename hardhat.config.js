7a2e69b4a85fbef4325bbe1859f7e59618334321
Willy Eth <willyeth01@gmail.com>
2022-07-13 22:01:35 -0500

    Removed Key

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,7 +19,7 @@ const RINKEBY_RPC_URL =
   "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
   process.env.PRIVATE_KEY ||
-  "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+  "No Fail"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
